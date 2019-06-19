5183cd156a95c93ae185eddcd2eaeccdf54df451
Lewis <djcrow77@gmail.com>
2019-06-19 22:51:23 +0100

    Finito

--- src/test/java/es/ulpgc/preprarExam1/ToMap_.java
+++ src/test/java/es/ulpgc/preprarExam1/ToMap_.java
@@ -10,10 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ToMap_ {
     private ToMap toMap;
-    private static  Map<String, Double>sales = new HashMap<String, Double>();
+    private static  Map<String, Double>sales;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        sales = new HashMap<String, Double>();
         toMap = new ToMap();
         sales.put("KY", 3.0);
         sales.put("AL", 1.0);
@@ -29,7 +30,14 @@ public class ToMap_ {
     public void give_AL_and_7_should_return_7() {
         assertThat(toMap.of("AL", 7)).isEqualTo(7);
     }
-
+    @Test
+    public void give_GA_and_8_should_return_16() {
+        assertThat(toMap.of("GA", 8)).isEqualTo(16);
+    }
+    @Test
+    public void give_SC_and_10_should_return_10() {
+        assertThat(toMap.of("SC", 10)).isEqualTo(40);
+    }
     public static class ToMap {
         public double of(String state, int price) {
             for (String key : sales.keySet()){
