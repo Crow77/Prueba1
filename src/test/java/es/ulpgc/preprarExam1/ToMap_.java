1e5295b880fb25b7fbdd94f0e75026770c622107
Lewis <djcrow77@gmail.com>
2019-06-19 22:00:35 +0100

    given_KY_and_4_shuld_return_12

--- src/test/java/es/ulpgc/preprarExam1/ToMap_.java
+++ src/test/java/es/ulpgc/preprarExam1/ToMap_.java
@@ -1,4 +1,37 @@
 package es.ulpgc.preprarExam1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ToMap_ {
+    private ToMap toMap;
+    private static  Map<String, Double>sales = new HashMap<String, Double>();
+
+    @Before
+    public void setUp() throws Exception {
+        toMap = new ToMap();
+        sales.put("KY", 3.0);
+        sales.put("AL", 1.0);
+        sales.put("GA", 2.0);
+        sales.put("SC", 4.0);
+    }
+
+    @Test
+    public void give_KY_and_4_should_return_12() {
+        assertThat(toMap.of("KY", 4)).isEqualTo(12);
+    }
+
+    public static class ToMap {
+        public double of(String state, int price) {
+            for (String key : sales.keySet()){
+                if (key.equals(state)) return price*sales.get(key);
+            }
+            return 0;
+        }
+    }
 }
